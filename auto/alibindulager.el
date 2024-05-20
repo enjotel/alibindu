2bbf57b0edb271baa0c0265b09831ff1c66658ff
enjotel <nilsliersch@web.de>
2024-05-21 00:57:13 +0200

    continued with Siddhayoga and imprved edition until after cakras and avadhuta section

--- auto/alibindulager.el
+++ auto/alibindulager.el
@@ -1,13 +1,13 @@
 (TeX-add-style-hook
  "alibindulager"
  (lambda ()
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "preamble"
@@ -83,8 +83,10 @@
     "starstrans"
     "ascetics"
     "attributesrajabody"
-    "avadhuta"
     "lotusofsupport"
-    "heartlotus"))
+    "heartlotus"
+    "heartlotustrans"
+    "knowledge"
+    "knowledgetrans"))
  :latex)
 
