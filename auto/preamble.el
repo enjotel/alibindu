3f663553088e40827ce8f4e473fa81d31dacf77c
enjotel <nilsliersch@web.de>
2026-09-09 09:40:29 +0200

    auf dem Weg zur Druckreife ganz weit

--- auto/preamble.el
+++ auto/preamble.el
@@ -5,13 +5,13 @@
                      '(("memoir" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("hyphenat" "htt") ("babel" "english") ("caption" "singlelinecheck=false") ("xcolor" "dvipsnames") ("csquotes" "autostyle") ("nowidow" "defaultlines=2" "all") ("biblatex" "backend=biber" "sorting=nyt" "maxnames=99" "style=verbose" "doi=true" "isbn=false" "url=true") ("ekdosis" "teiexport=tidy" "parnotes=true") ("hyperref" "hyperfootnotes=falsecolorlinks=true" "linkcolor=black" "citecolor=black" "urlcolor=black")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -63,6 +63,7 @@
     '("sectionmark" 1)
     '("chaptermark" 1)
     "TotalPrintedHeight"
+    "halfdanda"
     "normalpipe"
     "phantomsection"
     "fnsep"
